9adb81483f2ccc6524748df96269e09909833244
Cesar Osorio <Cesaros@users.noreply.github.com>
2023-04-08 19:16:02 -0400

    initial version - microsoft

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        message: 'Hi, what would you like to learn about Microsoft annual reports?',
         type: 'apiMessage',
       },
     ],
@@ -165,7 +165,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Ask any question about the annual reports of  Microsoft (2020-2022)
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -282,7 +282,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'What is Microsoft net income in 2022?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -319,8 +319,9 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          <a href="https://twitter.com/cesarosoriof">
+            Powered by LangChainAI. Demo built by Cesar (Twitter: @cesarosoriof)
+            using demos built by Mayo and frontend by Zahid
           </a>
         </footer>
       </Layout>
